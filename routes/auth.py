a131658faf0bff9565794905e75a2ca975653c46
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-10-20 17:11:26 +0530

    feat: auth routes

--- routes/auth.py
+++ routes/auth.py
@@ -15,6 +15,30 @@ async def signup(user: User):
             status_code=201, content={"message": "User signed up successfully"}
         )
     except Exception:
-        raise HTTPException(
-            status_code=409, detail="User already exists"
+        raise HTTPException(status_code=409, detail="User already exists")
+
+
+@router.post("/login")
+async def login(user: User):
+    try:
+        supabase.auth.sign_in_with_password(
+            {"email": user.email, "password": user.password}
         )
+        token = supabase.auth.get_session().access_token
+        return JSONResponse(
+            status_code=200,
+            content={"message": "User logged in successfully", "token": token},
+        )
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+
+@router.get("/user")
+async def get_users():
+    try:
+        token = supabase.auth.get_session().access_token
+        user = supabase.auth.get_user(token)
+        return user
+    except Exception:
+        raise HTTPException(status_code=401, detail="Unauthorized User")
+
