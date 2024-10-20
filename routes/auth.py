1337d157a43d73dcc63b2b75ef88c2615997729e
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-10-20 17:50:29 +0530

    feat: auth routes

--- routes/auth.py
+++ routes/auth.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from utilities.database import supabase
-from schema.schema import User
+from schema.schema import User, UpdateUser, UpdatePassword
 from utilities.response import JSONResponse
+from dotenv import load_dotenv
+import os
 
 router = APIRouter()
 supabase = supabase()
+load_dotenv()
 
 
 @router.post("/signup")
@@ -42,3 +45,33 @@ async def get_users():
     except Exception:
         raise HTTPException(status_code=401, detail="Unauthorized User")
 
+
+@router.post("/update")
+async def update_user(user: UpdateUser):
+    try:
+        user = supabase.auth.update_user({"email": user.email})
+        return user
+    except Exception:
+        raise HTTPException(status_code=401, detail="Unauthorized User")
+
+
+@router.post("/logout")
+async def logout():
+    try:
+        supabase.auth.sign_out()
+        return JSONResponse(
+            status_code=200, content={"message": "User logged out successfully"}
+        )
+    except Exception:
+        raise HTTPException(status_code=401, detail="Unauthorized User")
+
+
+@router.post("/reset")
+async def reset_password(password: UpdatePassword):
+    try:
+        supabase.auth.update_user({"password": password.password,"nonce": os.getenv("NONCE")})
+        return JSONResponse(
+            status_code=200, content={"message": "Password reset successfully"}
+        )
+    except Exception:
+        raise HTTPException(status_code=401, detail="Unauthorized User")
--- schema/schema.py
+++ schema/schema.py
@@ -4,3 +4,9 @@ from pydantic import BaseModel
 class User(BaseModel):
     email: str
     password: str
+
+class UpdateUser(BaseModel):
+    email: str
+
+class UpdatePassword(BaseModel):
+    password: str
